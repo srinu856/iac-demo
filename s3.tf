588ad6a68776d2f49406364674387aaf62da7882
srinu856 <101277128+srinu856@users.noreply.github.com>
2023-07-25 13:50:39 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "sujatha27"
+
+
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = "sujatha27"
+  key    = "vpc.tf"
+  source = "${path.module}/vpc.tf"
+  etag   = "${filemd5("${path.module}/vpc.tf")}"
+}
+
