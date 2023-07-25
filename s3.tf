a54a510dcc6b0924a890bb1c21fd71fa38385207
srinu856 <101277128+srinu856@users.noreply.github.com>
2023-07-25 13:54:08 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "sujatha27"
+  bucket = "sujatha276"
 
 
 }
 
 resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "sujatha27"
+  bucket = "sujatha276"
   key    = "vpc.tf"
   source = "${path.module}/vpc.tf"
   etag   = "${filemd5("${path.module}/vpc.tf")}"
